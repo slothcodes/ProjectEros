2bc61c2e5aeeaf6eec92aabde9ecd45cf9d040b0
slothcodes <william.alex.davis@gmail.com>
2022-06-10 02:57:30 -0600

    Add files via upload

--- dataBuilder.js
+++ dataBuilder.js
@@ -9,21 +9,21 @@ const fs = require('fs');
 const RedTube = require('./redTubeAPIscraper')
 const Eporner = require('./epornScraper.js')
 
-let ToDoKeywords = ['boobs'];
+let ToDoKeywords = ['asian'];
 let usedKeyWords = [];
 let allResults = [];
 
 async function redSearch(){
-    for (x=0;x<ToDoKeywords.length;x++){
+        for (x=0;x<ToDoKeywords.length;x++){
         let results = await redIterate(ToDoKeywords[0])
         usedKeyWords.push(ToDoKeywords[0])
         ToDoKeywords.splice(0,1)
         saveResultsAppend(results,'results','');
         saveKeywords(ToDoKeywords,'ToDoKeywords')
         saveKeywords(usedKeyWords,'usedKeywords')
-    };    
-    return 
+        };    
 }
+
 
 async function ESearch(){
     for (x=0;x<ToDoKeywords.length;x++){
@@ -76,30 +76,38 @@ async function redIterate(searchKW){
         for (let x=1;x< numberOfPages;x++){
             statusUpdate(searchKW,x,numberOfPages);
             //console.log(resultList.length)
-            let searchResults = await RedTube.searchRedTube(searchKW,[searchKW],x)
-            // Split Below Into New Function
-            // INCLUDE NONLATIN AND DUPE CHECK FUNCTIONS IN NEW FUNCTION FOR BELOW
-            searchResults.videos.forEach(data =>{
-                let resultString = data.video.title.replace(/\s\s+/g, ' ')
-                // IF NON LATIN CHARS SKIP
-                
-                data.video.tags.forEach(element =>{
-                    checkUsedKeywords(element.tag_name)
-                    resultString = resultString + ' ' + element.tag_name
-                        // IF DUPE SKIP                
-                })  
+            try{
+                let searchResults = await RedTube.searchRedTube(searchKW,[searchKW],x)
+                // Split Below Into New Function
+                // INCLUDE NONLATIN AND DUPE CHECK FUNCTIONS IN NEW FUNCTION FOR BELOW
+                searchResults.videos.forEach(data =>{
+                    let resultString = data.video.title.replace(/\s\s+/g, ' ')
+                    // IF NON LATIN CHARS SKIP
+                    
+                    data.video.tags.forEach(element =>{
+                        checkUsedKeywords(element.tag_name)
+                        resultString = resultString + ' ' + element.tag_name
+                            // IF DUPE SKIP                
+                    })  
 
-                if (nonLatinCheck(resultString)){ 
-                    //console.log('pass latin test ' + resultString )   
-                    if(!checkDupes(resultString)){ 
-                        //console.log('no dupes')
-                        resultList.push(resultString)// + '\n')
-                        allResults.push(resultString)// + '\n')
+                    if (nonLatinCheck(resultString)){ 
+                        //console.log('pass latin test ' + resultString )   
+                        if(!checkDupes(resultString)){ 
+                            //console.log('no dupes')
+                            resultList.push(resultString)// + '\n')
+                            allResults.push(resultString)// + '\n')
+                        }
+
                     }
-
-                }
-                
-            });
+                    
+                });
+            }
+            catch(e){
+                console.error(e.name + ': ' + e.message)
+                const errorMess = e.message;
+                saveErrorFile(e.message + '\n' + e + '\n' + Object.keys(searchResults),'errorFile')
+                return resultList
+            }
         }
     }
 
@@ -120,7 +128,8 @@ async function ePornerIterate(searchKW){
             statusUpdate(searchKW,x,numberOfPages);
             //console.log(resultList.length)
             let searchResults = await RedTube.searchRedTube(searchKW,[searchKW],x)
-            //console.log('search results \n' + searchResults)
+            console.log('search results \n' + Object.keys(searchResults))
+            console.log('search results \n' + searchResults.message)
             // Split Below Into New Function
             // INCLUDE NONLATIN AND DUPE CHECK FUNCTIONS IN NEW FUNCTION FOR BELOW
             searchResults.videos.forEach(data =>{
@@ -165,6 +174,7 @@ function saveResultsAppend(results,name,seperator){
 function saveKeywords(keywords,name){
     let writeStream = fs.createWriteStream(name + '.txt');
     writeStream.write(keywords.join('\n'),'utf-8');
+    //writeStream.end();
 }
 
 function loadSaveData(filename){
@@ -181,10 +191,10 @@ function ReInit(){
     allResults = loadSaveData('results.txt')
 }
 
-function saveFile(combinedResults,name,seperator){
+function saveErrorFile(errorMessage,name,seperator){
     let writeStream = fs.createWriteStream(name + '.txt');
-    console.log('writing file');
-    writeStream.write(combinedResults.join(seperator),'utf-8');
+    console.log('writing error file');
+    writeStream.write(errorMessage,'utf-8');
 }
 
 //ReInit()
